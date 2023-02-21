fc8e1a2febb37f3724d6c6a3484c0efb6851ee7e
Serhii Reznichenko <81872621+Sennarion@users.noreply.github.com>
2023-02-21 13:54:20 +0200

    final

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from 'redux/example/store';
-import './styles/index.css';
+import 'styles/index.css';
 import 'modern-normalize/modern-normalize.css';
-import App from './components/App';
+import App from 'components/App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
