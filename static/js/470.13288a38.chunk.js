b370b0a524283fab055d63cfa7e9c66d66ea4d16
Serhii Reznichenko <81872621+Sennarion@users.noreply.github.com>
2023-02-21 12:16:52 +0200

    Updates

---
+++ static/js/470.13288a38.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_ts_template=self.webpackChunkreact_ts_template||[]).push([[470],{470:function(t,e,u){u.r(e);u(791);var a=u(184);e.default=function(){return(0,a.jsx)("div",{children:"About"})}}}]);
+//# sourceMappingURL=470.13288a38.chunk.js.map
